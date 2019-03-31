5e5547f017a921888befcaf3f87e35c1c0532e2e
Dhruv <dhruvchitkara27@gmail.com>
2019-03-31 16:27:39 +0530

    Adjusting things

--- Full-scale-apps/course-purchase/src/App.js
+++ Full-scale-apps/course-purchase/src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
 
 class App extends Component {
   render() {
+    var courses = [
+      { name: 'Complete iOS10 dev course', price: 199 },
+      { name: 'Complete pentesting course', price: 299 },
+      { name: 'Complete front end dev course', price: 180 },
+      { name: 'Big Bounty and web app pentesting', price: 190 }
+    ];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Welcome to Course Purchase Page</h2>
+          <Coursesales items={courses}/>
         </header>
       </div>
     );
---
+++ Full-scale-apps/course-purchase/src/Coursesales.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+  render() {
+    console.log(this.props.items);
+    return (
+      <div>
+        I am from Coursesales Component.
+      </div>
+    );
+  }
+}
+export default Coursesales;
