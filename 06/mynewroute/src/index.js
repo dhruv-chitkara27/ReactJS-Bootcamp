20c49aecfb5d8e8c3d4f1b10eb28ee50fca56110
Dhruv <dhruvchitkara27@gmail.com>
2019-04-01 15:45:13 +0530

    Removing survey app and moving to another repository

---
+++ 06/mynewroute/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Home from './Home';
+import Contact from './Contact';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+ReactDOM.render(
+  <Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/contact">Contact-me</Link></li>
+    </ul>
+    <hr />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/contact" component={Contact} />
+
+
+  </div>
+</Router>
+, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
