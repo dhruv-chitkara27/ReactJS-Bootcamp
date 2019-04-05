ec8acf8de974fe883fc96824daa6b080cd50807b
Dhruv <dhruvchitkara27@gmail.com>
2019-04-05 20:16:30 +0530

    API Basics

---
+++ Full-scale-apps/auth0-github-api/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Github from './Github';
+import Header from './Components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Github />
+      </div>
+    );
+  }
+}
+
+export default App;
