f3978f2d550ebadb5820cccf2267153adf1adcb9
Dhruv <dhruvchitkara27@gmail.com>
2019-04-10 16:03:48 +0530

    Just finishing things

--- Full-scale-apps/auth0-github-api/src/Components/Profile.js
+++ Full-scale-apps/auth0-github-api/src/Components/Profile.js
@@ -11,7 +11,7 @@ class Profile extends Component {
     let repos = `${userdata.homeURL}/repositories`;
 
 
-    if(userdata.notFound === 'User not Found'){
+    if(userdata.message === 'Not Found'){
     return (
       <div className="notfound">
         <h2>Heyyyy</h2>
@@ -21,9 +21,24 @@ class Profile extends Component {
 }
     else {
       return (
-        <div>
-
-        </div>
+        <section className="github-profile">
+          <div className="github-profile-info">
+            <a href={userdata.homeUrl} target="_blank" title={userdata.name || userdata.username}><img src={userdata.avatar} /></a>
+            <h2><a href={userdata.homeUrl} title={userdata.username} target="_blank">{userdata.name || userdata.username}</a></h2>
+            <h3>{userdata.location}</h3>
+          <ul>
+              <li>
+                <a href={followers} target="_blank" title="Number of Followers"><i>{userdata.followers}</i><span>Followers</span></a>
+              </li>
+              <li>
+                <a href={repos} target="_blank" title="Number of Repositories"><i>{userdata.repos}</i><span>Repositories</span></a>
+              </li>
+              <li>
+                <a href={following} target="_blank" title="Number of Following"><i>{userdata.following}</i><span>Following</span></a>
+              </li>
+            </ul>
+          </div>
+        </section>
       );
     }
   }
