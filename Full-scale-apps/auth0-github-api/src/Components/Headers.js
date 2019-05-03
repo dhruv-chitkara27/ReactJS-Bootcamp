5eba41c1bfe1b62597c0c10bb61a2c2a794a49ed
Dhruv <dhruvchitkara27@gmail.com>
2019-05-03 23:41:09 +0530

    Other Component

--- Full-scale-apps/auth0-github-api/src/Components/Headers.js
+++ Full-scale-apps/auth0-github-api/src/Components/Headers.js
@@ -31,16 +31,6 @@ class Headers extends Component {
     </Nav>
   </Navbar.Collapse>
 </Navbar>
-      // <Navbar>
-      //   <Navbar.Header>
-      //     <Navbar.Brand>
-      //       "Github Searcher"
-      //     </Navbar.Brand>
-      //   </Navbar.Header>
-      //   <Nav>
-      //     <NavItem onClick={this.onLogin.bind(this)} href="#"> Login</NavItem>
-      //   </Nav>
-      // </Navbar>
     );
   }
 }
